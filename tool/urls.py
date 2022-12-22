31b0257fa37345db88cf61c32869cd4354fcec72
aggarwal.jaggarwal@gmail.com <aggarwal.jaggarwal@gmail.com>
2022-12-22 19:21:20 +0530

    day3

--- tool/urls.py
+++ tool/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('home',views.home,name='home'),
+    path('drive_home',views.drive_home,name='drive_home'),
     path('csv_home',views.csv_home,name='csv_home'),
-    path('pdf_home',views.pdf_home,name='pdf_home')
+    path('pdf_home',views.pdf_home,name='pdf_home'),
+    
+    #csv
+    path('csv_edit',views.csv_edit,name='csv_edit')
 ]
--- tool/views.py
+++ tool/views.py
@@ -5,8 +5,16 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'home.html')
 
+def drive_home(request):
+    return render(request,'drive/drive_home.html')
+
 def csv_home(request):
     return render(request,'csv/csv_home.html')
 
 def pdf_home(request):
     return render(request,'pdf/pdf_home.html')
+
+#csv
+ 
+def csv_edit(request):
+    return render(request,'csv/csv_edit.html')
