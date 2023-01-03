4a86a19baceb0742db967a676b01ae4428ef4f48
aggarwal.jaggarwal@gmail.com <aggarwal.jaggarwal@gmail.com>
2023-01-03 21:37:11 +0530

    day4

---
+++ static/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".folder").click(function () {
+        // console.log($(this).attr('value'));
+        $.ajax({
+            url: '',
+            type: 'get',
+            data:{
+                button_text: $(this).attr('value')
+            },
+            success:function(response){
+                $("#files").empty();
+                const files = response.FileStatuses.FileStatus;
+                for (file in files) {
+                    const file_content = files[file];
+                    if (file_content["type"] == 'DIRECTORY') {
+                        $("#files").append('<div class="col-sm-1 m-2 p-2 folder" value="'+file_content["pathSuffix"]+'"><i class="fa fa-folder" aria-hidden="true" style="font-size: 50px; color:rgb(95,99,104);"></i><div class="text-truncate">'+file_content["pathSuffix"]+'</div></div>');
+                    } else {
+                        $("#files").append('<div class="col-sm-1 m-2 p-2" value="'+file_content["pathSuffix"]+'"><i class="fa fa-file-text" aria-hidden="true" style="font-size: 50px; color: rgb(25,103,210);" ></i><div class="text-truncate">'+file_content["pathSuffix"]+'</div></div>');
+                    }
+                    // text += person[x];
+                }
+            }
+        });
+    });
+});
--- tool/views.py
+++ tool/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import requests
+
+
+#ajax handler
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 # Create your views here.
 
 def home(request):
     return render(request,'home.html')
 
+#drive_home
+
 def drive_home(request):
-    return render(request,'drive/drive_home.html')
+    if is_ajax(request=request):
+        text = request.GET.get('button_text')
+        response = requests.get("http://localhost:9870/webhdfs/v1/user/hadoop/"+text+"?op=LISTSTATUS")
+        context = response.json()
+        return JsonResponse(context,status=200)
+    
+    response = requests.get("http://localhost:9870/webhdfs/v1/user/hadoop?op=LISTSTATUS")
+    context = response.json()
+    return render(request,'drive/drive_home.html',context)
 
 def csv_home(request):
     return render(request,'csv/csv_home.html')
@@ -18,3 +34,6 @@ def pdf_home(request):
  
 def csv_edit(request):
     return render(request,'csv/csv_edit.html')
+
+
+
