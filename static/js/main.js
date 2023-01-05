6ec4d9201be9defca615ccf6d328740b858b2fd9
aggarwal.jaggarwal@gmail.com <aggarwal.jaggarwal@gmail.com>
2023-01-05 18:31:50 +0530

    folder creation

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     //======================DRIVE=========================
     $('body').on('click' , '.folder',function () {
-        console.log($(this).attr('value'));
         $.ajax({
             url: '',
             type: 'get',
@@ -25,12 +24,35 @@ $(document).ready(function(){
     });
 
     $('body').on('click' , '.back_folder',function () {
-        console.log($(this).attr('value'));
         $.ajax({
             url: '',
             type: 'get',
             data:{
                 action: "back"
+            },
+            success:function(response){
+                $("#files").empty();
+                const files = response.FileStatuses.FileStatus;
+                for (file in files) {
+                    const file_content = files[file];
+                    if (file_content["type"] == 'DIRECTORY') {
+                        $("#files").append('<div class="col-sm-1 m-2 p-2 folder" value="'+file_content["pathSuffix"]+'"><i class="fa fa-folder" aria-hidden="true" style="font-size: 50px; color:rgb(95,99,104);"></i><div class="text-truncate">'+file_content["pathSuffix"]+'</div></div>');
+                    } else {
+                        $("#files").append('<div class="col-sm-1 m-2 p-2" value="'+file_content["pathSuffix"]+'"><i class="fa fa-file-text" aria-hidden="true" style="font-size: 50px; color: rgb(25,103,210);" ></i><div class="text-truncate">'+file_content["pathSuffix"]+'</div></div>');
+                    }
+                    // text += person[x];
+                }
+            }
+        });
+    });
+
+    $('body').on('click' , '.new_folder',function () {
+        $.ajax({
+            url: '',
+            type: 'get',
+            data:{
+                action: "new_folder",
+                dir_name: $('.dir_name').val()
             },
             success:function(response){
                 $("#files").empty();
--- tool/views.py
+++ tool/views.py
@@ -13,6 +13,7 @@ def home(request):
 
 #==========================================DRIVE_HOME=================================================================
 current_path=""
+localhost="http://localhost:9870/"
 def drive_home(request):
     global current_path
     if is_ajax(request=request):
@@ -22,10 +23,10 @@ def drive_home(request):
         if action == "open":
             dir = request.GET.get('button_text')
             if current_path == "":
-                response = requests.get("http://localhost:9870/webhdfs/v1/user/hadoop/"+dir+"?op=LISTSTATUS")
+                response = requests.get(localhost+"webhdfs/v1/user/hadoop/"+dir+"?op=LISTSTATUS")
                 current_path = dir
             else:
-                response = requests.get("http://localhost:9870/webhdfs/v1/user/hadoop/"+current_path+"/"+dir+"?op=LISTSTATUS")
+                response = requests.get(localhost+"webhdfs/v1/user/hadoop/"+current_path+"/"+dir+"?op=LISTSTATUS")
                 current_path = current_path+"/"+dir
             context = response.json()
             return JsonResponse(context,status=200)
@@ -39,15 +40,30 @@ def drive_home(request):
                 current_path=l[0]
 
             if current_path == "":
-                response = requests.get("http://localhost:9870/webhdfs/v1/user/hadoop?op=LISTSTATUS")
+                response = requests.get(localhost+"webhdfs/v1/user/hadoop?op=LISTSTATUS")
             else:
-                response = requests.get("http://localhost:9870/webhdfs/v1/user/hadoop/"+current_path+"?op=LISTSTATUS")
+                response = requests.get(localhost+"webhdfs/v1/user/hadoop/"+current_path+"?op=LISTSTATUS")
+            context = response.json()
+            return JsonResponse(context,status=200)
+        
+        elif action == "new_folder":
+            dir_name = request.GET.get('dir_name')
+            if current_path == "":
+                response = requests.put(localhost+"webhdfs/v1/user/hadoop/"+dir_name+"?user.name=hadoop&op=MKDIRS")
+            else:
+                response = requests.put(localhost+"webhdfs/v1/user/hadoop/"+current_path+"/"+dir_name+"?user.name=hadoop&op=MKDIRS")
+
+
+            if current_path == "":
+                response = requests.get(localhost+"webhdfs/v1/user/hadoop?op=LISTSTATUS")
+            else:
+                response = requests.get(localhost+"webhdfs/v1/user/hadoop/"+current_path+"?op=LISTSTATUS")
             context = response.json()
             return JsonResponse(context,status=200)
 
     else:
         current_path=""
-        response = requests.get("http://localhost:9870/webhdfs/v1/user/hadoop?op=LISTSTATUS")
+        response = requests.get(localhost+"webhdfs/v1/user/hadoop?op=LISTSTATUS")
         context = response.json()
         return render(request,'drive/drive_home.html',context)
 
